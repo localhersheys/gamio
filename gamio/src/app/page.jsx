8a0e572bc1c158e1f0c2167dc7b014641c823373
omansh <omaansh@gmail.com>
2024-04-11 11:48:42 +0530

    feat: spaceship added

--- gamio/src/app/page.jsx
+++ gamio/src/app/page.jsx
@@ -5,7 +5,7 @@ const Home = () => {
             <div className="flex flex-wrap justify-between gap-20">
             <Card name="Game 1"  desc="lorem ipsum ksajfiofnsao ajs dfs go fwaefw eufnweuf" thumbnail={"/game.png"} id={1}></Card>
             <Card name="Game 2"  desc="lorem ipsum ksajfiofnsao ajs dfs go fwaefw eufnweuf" thumbnail={"/game.png"} id={2}></Card>
-            <Card name="Game 3"  desc="lorem ipsum ksajfiofnsao ajs dfs go fwaefw eufnweuf" thumbnail={"/game.png"} id={3}></Card>
+            <Card name="Space Shooter"  desc="lorem ipsum ksajfiofnsao ajs dfs go fwaefw eufnweuf" thumbnail={"/game.png"} id={3}></Card>
             </div>
         </main>
      );
---
+++ gamio/src/components/game/3/Ship.jsx
@@ -0,0 +1,19 @@
+const Ship = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -0.5 32 32"
+      shape-rendering="crispEdges"
+    >
+      <metadata>
+        Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj
+      </metadata>
+      <path
+        stroke="#f97316"
+        d="M14 5h1M13 6h1M15 6h1M13 7h1M15 7h1M13 8h1M15 8h1M13 9h1M15 9h1M13 10h3M13 11h1M15 11h1M13 12h1M15 12h1M7 13h1M12 13h2M15 13h2M21 13h1M12 14h1M14 14h1M16 14h1M7 15h1M12 15h1M14 15h1M16 15h1M21 15h1M7 16h1M12 16h1M14 16h1M16 16h1M21 16h1M7 17h1M12 17h2M15 17h2M21 17h1M7 18h1M11 18h1M13 18h1M15 18h1M17 18h1M21 18h1M7 19h1M9 19h2M13 19h1M15 19h1M18 19h2M21 19h1M7 20h2M11 20h1M13 20h1M15 20h1M17 20h1M20 20h2M7 21h1M11 21h1M13 21h1M15 21h1M17 21h1M21 21h1M7 22h1M11 22h1M17 22h1M21 22h1M7 23h5M13 23h3M17 23h5M12 24h1M16 24h1"
+      />
+    </svg>
+  );
+};
+
+export default Ship;
