cde06b9e9d272e6830b2259f3d929cf630e23da2
harshita <harshita.shanker2022@vitstudent.ac.in>
2024-04-24 16:52:12 +0530

    completed basic game 2

--- gamio/src/app/game/page.jsx
+++ gamio/src/app/game/page.jsx
@@ -20,7 +20,7 @@ const Home = () => {
         <main className="bg-black p-20 h-[100vh] w-[100vw]" style={{ backgroundImage: `url(/bgImage.png)`, filter: `contrast(120%)`}}>
             <div className="flex flex-wrap justify-between gap-20">
             <Card name="Game 1"  desc="lorem ipsum ksajfiofnsao ajs dfs go fwaefw eufnweuf" thumbnail={"/game.png"} id={1}></Card>
-            <Card name="Game 2"  desc="lorem ipsum ksajfiofnsao ajs dfs go fwaefw eufnweuf" thumbnail={"/game.png"} id={2}></Card>
+            <Card name="Tic Tac Toe"  desc="playing tic tac toe is an integral part of every indian's school life. but can the computer beat you at your own forte? or will you show the world that computers are still 100 years too young? play against the computer to test your wits in a classic game of tic tac toe. " thumbnail={"/game.png"} id={2}></Card>
             <Card name="Space Wars"  desc={`Player 1 uses W/S to move and Left Shift to fire. \nPlayer 2 uses Up/Down arrows to move and Right Shift to shoot. \nclassic arcade-style two-player game where you pilot spaceships and battle it out. Take control and shoot down your opponent's ship before they destroy yours.  \nLast spaceship standing wins this cosmic duel! Test your reflexes in fast-paced, head-to-head combat for galactic supremacy.`} thumbnail={"/spaceShooter.jpeg"} id={3}></Card>
             </div>
         </main>
