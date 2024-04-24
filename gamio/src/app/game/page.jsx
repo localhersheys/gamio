b79646d587d9bde6d78ee31f8b4bedea9458851e
omansh <omaansh@gmail.com>
2024-04-24 15:20:32 +0530

    messing with border'

--- gamio/src/app/game/page.jsx
+++ gamio/src/app/game/page.jsx
@@ -18,9 +18,9 @@ const Home = () => {
         };
       }, []);
     return ( 
-        <main className="bg-black p-20 h-[100vh] w-[100vw]" style={{ backgroundImage: `url(/bgImage.png)`, filter: `contrast(120%)`}}>
+        <main className="bg-black p-20 h-[100vh] w-[100vw]" style={{ backgroundImage: `url(/bgImage.png)`, backgroundSize:"cover"}}>
             <div className="flex flex-wrap justify-between gap-20">
-            <Card2 name="Game 1"  desc="lorem ipsum ksajfiofnsao ajs dfs go fwaefw eufnweuf" thumbnail={"/game.png"} id={1}></Card2>
+            <Card2 name="Game 1"  desc="Fruit Crush is a delightful puzzle game where players match rows or columns of three or more fruits of the same type to crush them and earn points. As you progress, you can strategically align four or even five fruits to create powerful combos and rack up even more points" thumbnail={"/fruit.png"} id={1}></Card2>
             <Card name="Game 2"  desc="lorem ipsum ksajfiofnsao ajs dfs go fwaefw eufnweuf" thumbnail={"/game.png"} id={2}></Card>
             <Card name="Space Wars"  desc={`Player 1 uses W/S to move and Left Shift to fire. \nPlayer 2 uses Up/Down arrows to move and Right Shift to shoot. \nclassic arcade-style two-player game where you pilot spaceships and battle it out. Take control and shoot down your opponent's ship before they destroy yours.  \nLast spaceship standing wins this cosmic duel! Test your reflexes in fast-paced, head-to-head combat for galactic supremacy.`} thumbnail={"/spaceShooter.jpeg"} id={3}></Card>
             </div>
--- gamio/src/components/home/Card.jsx
+++ gamio/src/components/home/Card.jsx
@@ -3,22 +3,24 @@ import Link from "next/link";
 const Card = ({ name, desc, thumbnail, id }) => {
   return (
     <Link href={`/game/${encodeURIComponent(id)}`}>
+      
       <div
-        className="h-[80vh] w-[25vw] bg-orange-500 hover:scale-105 border border-white p-2 active:scale-90"
+        className="w-[405px] h-[600px] hover:scale-105 px-[70px] pt-[90px] pb-[132px] active:scale-90"
         style={{
-          backgroundImage: `url(/bgCard.jpg)`,
+          backgroundImage: `url(/bgCard.png)`,
           backgroundSize: "cover", // Ensure the image covers the entire div
           backgroundRepeat: "no-repeat", // Prevent the image from repeating
-          filter: "contrast(130%) brightness(120%)"
+          // filter: "brightness(120%)"
         }}
       >
-        <div className="w-full h-[53%] relative">
-          <Image src={thumbnail} layout="fill" objectFit="cover" />
+        <div className="w-full h-[190px] relative">
+          <Image src={thumbnail} layout="fill" objectFit="cover" className="rounded"/>
         </div>
         <div className="px-2 pt-1">
           <h1 className="font-press text-3xl text-black text-center">{name}</h1>
           <p className={`pt-2 overflow-auto h-[28vh] text-sm`}>{desc}</p>
         </div>
+        
       </div>
     </Link>
   );
